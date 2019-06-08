4f8b2fb30f40aac1b5d4ac9d870de307a617ecbc
Raghu Betina <raghu@firstdraft.com>
2019-06-08 20:38:41 +0000

    Generate recipes resource with draft:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # Routes for the Recipe resource:
+
+  # CREATE
+  get("/recipes/new", { :controller => "recipes", :action => "new_form" })
+  post("/create_recipe", { :controller => "recipes", :action => "create_row" })
+
+  # READ
+  get("/recipes", { :controller => "recipes", :action => "index" })
+  get("/recipes/:id_to_display", { :controller => "recipes", :action => "show" })
+
+  # UPDATE
+  get("/recipes/:prefill_with_id/edit", { :controller => "recipes", :action => "edit_form" })
+  post("/update_recipe/:id_to_modify", { :controller => "recipes", :action => "update_row" })
+
+  # DELETE
+  get("/delete_recipe/:id_to_remove", { :controller => "recipes", :action => "destroy_row" })
+
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
