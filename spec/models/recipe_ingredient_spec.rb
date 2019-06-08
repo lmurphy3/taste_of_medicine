401526ef90e0813194fa9e5a5775963da1f591ef
Raghu Betina <raghu@firstdraft.com>
2019-06-08 20:39:01 +0000

    Direct association between ingredients and recipe_ingredients

--- spec/models/recipe_ingredient_spec.rb
+++ spec/models/recipe_ingredient_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe RecipeIngredient, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:ingredient) }
+
     it { should belong_to(:recipe) }
 
     end
