398fbeb8d026a38ec9bba885d789024cbc3575ef
Raghu Betina <raghu@firstdraft.com>
2019-06-08 20:39:01 +0000

    Direct association between recipes and recipe_ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,9 @@
 class Recipe < ApplicationRecord
   # Direct associations
 
+  has_many   :recipe_ingredients,
+             :dependent => :destroy
+
   has_many   :favorite_recipes,
              :dependent => :destroy
 
